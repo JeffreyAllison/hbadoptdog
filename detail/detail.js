5b568637454b4692c7a24c01ddc35a5dfbbc6dc8
JeffAllison <itsjallison@gmail.com>
2022-03-21 14:04:45 -0700

    added window event listener and for loop to render dogcard in app jr, added getDogs and getDog(id)  function in fetchutlis js

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,13 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+window.addEventListener('load', async () => {
+
+  const data = new URLSearchParams(window.location.search);
+  const getDogId = data.get('id');
+
+
+});
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
